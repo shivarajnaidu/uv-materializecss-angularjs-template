15e07f1788f9b640649b0238d61e427fb62d97ee
yuvaraj <shivarajnaidu@gmail.com>
2017-04-08 10:57:21 +0530

    Added navActive Directive to highlight ActiveNavigation Link In Navigation Bar

---
+++ public/js/directives/nav-active.js
@@ -0,0 +1,29 @@
+;(function () {
+	angular.module('myApp')
+	.directive('navActive', ['$rootScope', '$window', navActive]);
+
+	function navActive($rootScope, $window) {
+
+		function link(scope, element, attrs) {
+			var li = element.parent();
+			if (li[0].nodeName === 'LI') {
+				$rootScope.$on('$routeChangeSuccess', function(e) {
+					if (element[0].href === $window.location.href) {
+						li.addClass('active');
+					} else {
+						li.removeClass('active');
+					}
+				})
+			}
+		}
+
+		return {
+			restrict: 'A',
+			link: link
+		}
+	}
+	
+
+})();
+
+
